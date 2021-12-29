3a96f2d7e94af6ca741627ab09dea91ba96bd540
leobuck <leobuck12@gmail.com>
2021-12-28 23:44:17 -0300

    Entidades

---
+++ Cadastro/Cadastro.Core/Domain.Model/Organizadores/Organizador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro.Core.Domain.Model.Organizadores
+{
+    public class Organizador
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; set; }
+
+        public Organizador()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
